783d5d20aa95587cecdf4ce009719a99f760811c
surprisedPikachu007 <arvind7447@gmail.com>
2023-05-23 22:45:20 +0530

    6 - Multi-Threading

---
+++ 6/app/src/main/java/com/example/sixthexperiment/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.sixthexperiment;
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity
+{
+    ImageView img;
+    Button bt1,bt2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Exercise 6");
+
+        bt1 = (Button)findViewById(R.id.button);
+        bt2= (Button) findViewById(R.id.button2);
+        img = (ImageView)findViewById(R.id.imageView);
+        bt1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                new Thread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        img.post(new Runnable()
+                        {
+                            @Override
+                            public void run()
+                            {
+                                img.setImageResource(R.drawable.aj1);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                new Thread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        img.post(new Runnable()
+                        {
+                            @Override
+                            public void run()
+                            {
+                                img.setImageResource(R.drawable.aj2);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+    }
+}
